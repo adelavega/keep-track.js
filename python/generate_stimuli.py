fd134fd7c453cd5a28b595094a4b5b91e36b7007
Alejandro de la Vega <delavega@colorado.edu>
2015-07-17 15:42:58 -0600

    Edited generation script so that easiest category always goes first, and so that last targets are in all caps in all stimuli list (only in csv)

--- python/generate_stimuli.py
+++ python/generate_stimuli.py
@@ -5,6 +5,23 @@ import json
 from os import makedirs
 from os.path import exists
 
+"""" Counterbalancing rules (per Naomi):
+
+Most important: 
+Each category used equally often as target. 
+each word used equally often as a target and distractor; each word also equally often as a final word.
+
+Additionally, I tried to have variability in which categories appear together (e.g., animals not always with countries). 
+
+There are systematic things about the lists: 
+With 3 categories, one has 1 target, one has 2, and one has 3. When there are 4 categories, the extra one can have any of those #s in it (tried to equate). When there are only 2, select from those #s evenly across trials (see the spreadsheet you have). 
+The last word in the trial is never a relevant word; looks like the first word can be relevant, but never is the final word for a category (though that may have been unintentional). 
+target words do not repeat across adjacent trials
+final words cannot repeat across adjacent trials
+distactors *can* repeat across adjacent trials
+distractors in one trial *can* be the same as target words in adjacent trials
+
+"""
 
 categories = {"Animals": ["Dog", "Cat", "Tiger", "Horse", "Lion", "Cow"], "Relatives": ["Sister", "Mother", "Brother", "Aunt", "Father", "Uncle"], "Distances": ["Mile", "Centimeter", "Inch", "Foot", "Meter", "Yard"], "Countries": [
     "Germany", "Russia", "Canada", "France", "England", "Mexico"], "Metals": ["Zinc", "Tin", "Steel", "Iron", "Copper", "Platinum"], "Colors": ["Red", "Green", "Blue", "Yellow", "Black", "Orange"]}
@@ -27,6 +44,10 @@ class StimGen():
 
 		random.shuffle(num_targets)
 
+		# Start with the easiest category always
+		while(num_targets[0] != pd.DataFrame(self.num_targets).min()[0]):
+			random.shuffle(num_targets)
+
 
 	def _select_trial_types(self):
 		## Select trial types
@@ -48,11 +69,14 @@ class StimGen():
 	def _choose_targets(self, trial_cats, last_targets = []):
 		trial_targets = []
 
+		if len(trial_cats) == 5:
+			choose_cats = [trial_cats[0]] + [trial_cats[1]] *2 + [trial_cats[2]] * 2 + [trial_cats[3]] * 3 + [trial_cats[4]] * 3
+
 		# Set how many to choose from each category
-		if len(trial_cats) > 2:
+		elif len(trial_cats) > 2:
 			choose_cats = [trial_cats[0]] + [trial_cats[1]] *2 + [trial_cats[2]] * 3
 
-			if len(trial_cats) == 4:
+			if len(trial_cats) >3:
 				n_last = random.choice([1, 2, 3])
 				choose_cats += [trial_cats[3]] * n_last
 		
@@ -60,7 +84,7 @@ class StimGen():
 		for cat in choose_cats:
 			# Try stimuli that have no been distractors first, up to those that have been distractors 10 times
 			found = False
-			for num_reps in range(3):
+			for num_reps in range(self.max_reps):
 				# Stim that have been distractors n times, and are in the category
 				avail_stim = self.target_dist_count[(self.target_dist_count.Words.isin(categories[cat])) & (self.target_dist_count.Target == num_reps)]
 				if len(avail_stim) > 0:
@@ -91,10 +115,8 @@ class StimGen():
 		for x in range(self.len_lists - len(targets) + 1):
 			found = False
 
-			max_reps = 3
-
-			for num_reps in range(max_reps):
-				avail_stim = self.target_dist_count[(self.target_dist_count.Words.isin(words_not) == False) & (self.target_dist_count.Distractor == num_reps)]
+			for num_reps in range(self.max_reps):
+				avail_stim = self.target_dist_count[(self.target_dist_count.Words.isin(words_not) == False) & (self.target_dist_count.Distractor < num_reps)]
 				if len(avail_stim) > 0:
 					stim = random.choice(list(avail_stim.Words))
 					max_i = len(avail_stim.Words)
@@ -127,7 +149,7 @@ class StimGen():
 		return sequence, {cat : filter(lambda x: x in categories[cat], sequence)[-1] for cat in trial_cats}
 
 
-	def generate_stim(self):
+	def generate_stim(self, max_reps):
 		""" Run this to generate the stimuli"""
 
 		## Put it all together
@@ -137,6 +159,7 @@ class StimGen():
 		self.last_targets = []
 		self.target_dist_count = pd.DataFrame({'Words': [item for sublist in [categories[cat] for cat in categories] for item in sublist], 'Distractor' : 0, 'Target': 0, 'Last': 0})
 
+		self.max_reps = max_reps
 
 		self._select_trial_types()
 
@@ -158,17 +181,21 @@ class StimGen():
 		if not exists(out_dir):
 			makedirs(out_dir)
 
-		pd.DataFrame(self.all_stimuli).T.to_csv(os.path.join(out_dir, 'stimuli.csv'))
-		pd.DataFrame(self.last_targets).T.to_csv(os.path.join(out_dir, 'last_targets.csv'))
-		pd.DataFrame(self.all_targets).T.to_csv(os.path.join(out_dir, 'all_targets.csv'))
-		pd.DataFrame(self.trial_types).T.to_csv(os.path.join(out_dir, 'categories.csv'))
-		pd.DataFrame(self.target_dist_count).to_csv(os.path.join(out_dir, 'counts.csv'))
-
 		js_data = []
 		for i, trial in enumerate(self.trial_types):
 			js_data.append([trial, self.last_targets[i], self.all_stimuli[i]])
 
 		json.dump(js_data, open(os.path.join(out_dir, 'stim.txt'), 'w'))
+
+		# Make last word upper case for csv
+		csv_all_stim = [[word.upper() if word in self.last_targets[n] else word for word in stim] for n, stim in enumerate(self.all_stimuli)]
+
+
+		pd.DataFrame(csv_all_stim).T.to_csv(os.path.join(out_dir, 'stimuli.csv'))
+		pd.DataFrame(self.last_targets).T.to_csv(os.path.join(out_dir, 'last_targets.csv'))
+		pd.DataFrame(self.all_targets).T.to_csv(os.path.join(out_dir, 'all_targets.csv'))
+		pd.DataFrame(self.trial_types).T.to_csv(os.path.join(out_dir, 'categories.csv'))
+		pd.DataFrame(self.target_dist_count).to_csv(os.path.join(out_dir, 'counts.csv'))
 
 
 
